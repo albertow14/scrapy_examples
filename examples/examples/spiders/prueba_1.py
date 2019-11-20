8ee4a6cb5dc4b98ddb645e1d6de86f8ed05d50c7
Python <python@ZiMac4-3.local>
2019-11-20 19:12:36 +0100

    cambiar

--- examples/examples/spiders/prueba_1.py
+++ examples/examples/spiders/prueba_1.py
@@ -8,13 +8,13 @@ class VinosCorteInglesSpider(scrapy.Spider):
     allowed_domains = ['elcorteingles.es']
     start_urls = ['https://www.elcorteingles.es/club-del-gourmet/vinos/espana/']
 
-    def __init__(self):
-        self.counter = 0
+    # def __init__(self):
+    #     self.counter = 0
+
+    # def parse(self, response):
+    #     self.after_parse(response)
 
     def parse(self, response):
-        self.after_parse(response)
-
-    def after_parse(self, response):
         urls = response.xpath('//h3[@class="info-name"]/a/@href').extract()
         # print(urls)
         for url in urls:
@@ -26,17 +26,20 @@ class VinosCorteInglesSpider(scrapy.Spider):
             yield{'titulo_ci': titulo_ci, 'precio_ci':precio_ci_uno}
         
         siguiente_pagina = response.xpath('//a[text()="Siguiente"]/@href').extract_first()
-        self.siguiente_url(siguiente_pagina, response)
+        urls_siguiente_pagina = response.urljoin(header + siguiente_pagina)
+        if urls_siguiente_pagina is not None:
+            yield scrapy.Request(response.urljoin(urls_siguiente_pagina))
+        # self.siguiente_url(siguiente_pagina, response)
         
         # extraes el culo de la url, que te da cuando pinchas en el boton siguiente, extra
 
-    def siguiente_url(self, tail_url, response):
-        self.counter += 1
-        HEADER = "https://www.elcorteingles.es"
-        urls_siguiente_pagina = response.urljoin(HEADER + tail_url)
-        print(urls_siguiente_pagina)
-        if self.counter <= 36:
-            self.after_parse(response)
+    # def siguiente_url(self, tail_url, response):
+    #     self.counter += 1
+    #     HEADER = "https://www.elcorteingles.es"
+    #     urls_siguiente_pagina = response.urljoin(HEADER + tail_url)
+    #     print(urls_siguiente_pagina)
+    #     if self.counter <= 36:
+    #         self.after_parse(response)
 
 
             # print(completas, "esto esta completo")
